5e1a248cadc0345fd06f99eb266bab1d8b1917b7
GooKv <gookven@gmail.com>
2021-02-02 02:34:12 +0700

    lombok pojos supported

--- src/test/java/me/gookven/swingx/generictable/DummyDto.java
+++ src/test/java/me/gookven/swingx/generictable/DummyDto.java
@@ -1,10 +1,20 @@
 package me.gookven.swingx.generictable;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import me.gookven.swingx.generictable.api.GenericTableColumn;
 
+@ToString
+@Getter
+@Setter
 class DummyDto {
-    private final Gender gender;
+    @GenericTableColumn(value = "id", visible = false)
     private long id;
+    /**
+     * Is final w/o setter
+     */
+    private final Gender gender;
     private String name;
     private int age;
     private Gender petGender;
@@ -13,53 +23,5 @@ class DummyDto {
         this.name = name;
         this.age = age;
         this.gender = gender;
-    }
-
-    @GenericTableColumn(value = "id", visible = false)
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public Gender getPetGender() {
-        return petGender;
-    }
-
-    public void setPetGender(Gender petGender) {
-        this.petGender = petGender;
-    }
-
-    @Override
-    public String toString() {
-        return "DummyDto{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", gender=" + gender +
-                ", petGender=" + petGender +
-                '}';
     }
 }
